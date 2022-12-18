bcba65094003238f6a023966eb15651b79e1b5c0
mcgear <michael.gearhardt@gmail.com>
2022-12-18 11:24:20 -0700

    Move stories local to controls

--- src/atoms/action/Action.stories.tsx
+++ src/atoms/action/Action.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import Action from '../../../atoms/action/Action';
+import Action from './Action';
 
 export default {
   title: 'Atoms/Action',
