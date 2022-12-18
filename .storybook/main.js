e4c8dd1faae7a8e4da9ba5dbe5bea18edd0d4ec7
mcgear <michael.gearhardt@gmail.com>
2022-12-18 08:46:30 -0700

    Working stories with tailwind

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,31 +1,12 @@
 const path = require('path');
-
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/preset-create-react-app',
-  ],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-webpack5',
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/preset-create-react-app'],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
   },
-  webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\,css&/,
-      use: [
-        {
-          loader: 'postcss-loader',
-          options: {
-            ident: 'postcss',
-            plugins: [require('tailwindcss'), require('autoprefixer')],
-          },
-        },
-      ],
-      include: path.resolve(__dirname, '../'),
-    });
-    return config;
-  },
+  docs: {
+    docsPage: 'automatic'
+  }
 };
