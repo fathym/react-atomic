b5b5925cce92889f55eaf0cd60b01414db14fb9d
mcgear <michael.gearhardt@gmail.com>
2022-12-21 08:54:51 -0700

    Try this

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
-export * from './common';
-export * from './atoms';
-export * from './molecules';
-export * from './organisms';
-export * from './templates';
-export * from './pages';
+import ActionModel from './atoms/action/ActionModel';
+
+export { ActionModel };
+
+// export * from './common';
+// export * from './atoms';
+// export * from './molecules';
+// export * from './organisms';
+// export * from './templates';
+// export * from './pages';
 
 // TODO:  Web Components
 
